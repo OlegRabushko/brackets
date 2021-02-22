1ad474d63a4e94a05849ef73b9feab19c76616d8
Oleg <oleg.rabyshko@mail.ru>
2021-02-22 16:49:37 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    let brackets = {};
+
+    if (str.length % 2 !== 0) return false;
+
+    bracketsConfig.flat().map((item, index, array) => {
+        if (index % 2 !== 0) {
+            brackets[array[index - 1]] = item;
+        }
+    });
+
+    for (let char of str) {
+        for (let key in brackets) {
+            if (char === key && !stack.includes(brackets[key])) {
+                stack.push(char);
+            } else if (
+                char === brackets[key] &&
+                stack[stack.length - 1] === key
+            ) {
+                stack.pop();
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
